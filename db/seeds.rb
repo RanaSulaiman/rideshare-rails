3b95f224a9fddd184b6523486fdfce29e80b5be3
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-04 16:01:57 -0700

    Seeding drivers.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.read("support/drivers.csv", {:headers => true, :header_converters => :symbol}).each do |driver|
+  args = {
+          :id => driver[0],
+          :name => driver[1],
+          :vin => driver[2]
+        }
+  Driver.create(args)
+  # binding.pry
+end
