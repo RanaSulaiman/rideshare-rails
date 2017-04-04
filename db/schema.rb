3691d86852716ce0d276f0d7c067059980e1097c
Kelly Souza <kellysouza99@gmail.com>
2017-04-04 14:27:32 -0700

    adds passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404211931) do
+ActiveRecord::Schema.define(version: 20170404212537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "passengers", force: :cascade do |t|
+    t.integer  "rider_id"
+    t.string   "name"
+    t.string   "phone_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.integer  "trip_id"
