010d69bfc4e59c4ce374c5f36013003726683f29
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-04 16:19:07 -0700

    Seeded drivers.csv, passengers.scv and trips.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,30 @@
 require "csv"
 
-CSV.read("support/drivers.csv", {:headers => true, :header_converters => :symbol}).each do |driver|
+CSV.read("support/drivers.csv", {:headers => true}).each do |driver|
   args = {
-          :id => driver[0],
+          :driver_id => driver[0],
           :name => driver[1],
           :vin => driver[2]
         }
   Driver.create(args)
-  # binding.pry
+end
+
+CSV.read("support/passengers.csv", {:headers => true}).each do |passenger|
+  args = {
+          :rider_id => passenger[0],
+          :name => passenger[1],
+          :phone_num => passenger[2]
+        }
+  Passenger.create(args)
+end
+
+CSV.read("support/trips.csv", {:headers => true}).each do |trip|
+  args = {
+          :trip_id => trip[0],
+          :driver_id => trip[1],
+          :rider_id => trip[2],
+          :date => trip[3],
+          :rating => trip[4]
+        }
+  Trip.create(args)
 end
