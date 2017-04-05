8294ebc4741c7cefbf50b08dd18fc73c838dc020
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-05 16:55:42 -0700

    passengers/edit, passengers/new done

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -8,20 +8,34 @@ class PassengersController < ApplicationController
   end
 
   def new
-
+    @passenger = Passenger.new
   end
 
   def edit
-
+    @passenger = Passenger.find(params[:id])
   end
 
   def update
-
+    passenger = Passenger.find(params[:id])
+    if passenger.update(passenger_params)
+      redirect_to passengers_path
+    end
   end
 
   def create
+  passenger = Passenger.create passenger_params
 
+  unless passenger.id == nil
+    redirect_to passengers_path
   end
+  end
+
+
+  private
+  def passenger_params
+    params.require(:passenger).permit(:name, :phone_num)
+  end
+
 
 
 
