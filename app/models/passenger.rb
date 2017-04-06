04a07578d8870c6d705af2b3b5cde7930c7de887
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-05 22:20:30 -0700

    List of trips for each passanger done

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,11 @@ class Passenger < ApplicationRecord
   has_many :trips
   has_many :drivers, :through => :trips
 
+
+  def show_trips
+    @passenger = Passenger.find(params[:id]).trips
+    redirect_to passenger_path(@passenger.id)
+  end
+
+
 end
