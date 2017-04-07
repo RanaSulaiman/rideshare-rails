63679ec6e488e098f9e5ba828728ed9349959bb1
Kelly Souza <kellysouza99@gmail.com>
2017-04-07 14:45:25 -0700

    Adds driver average rating

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -12,4 +12,18 @@ class Driver < ApplicationRecord
     end
     return total
   end
+
+  def avg_rating
+    total = 0
+    num_of_ratings = 0
+    self.trips.each do |trip|
+      if trip.rating != nil
+        total += trip.rating
+        num_of_ratings += 1
+      end
+    end
+      return (total/num_of_ratings.to_f).round(1)
+
+  end
+
 end
