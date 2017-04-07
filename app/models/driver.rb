6d3baf212de77cced28b0a5a7048b285cd4197cf
Kelly Souza <kellysouza99@gmail.com>
2017-04-07 14:18:39 -0700

    Working on driver cost

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,15 @@
 class Driver < ApplicationRecord
   has_many :trips
   has_many :passengers, :through => :trips
+
+
+  def all_trips_cost
+    total = 0
+    self.trips.each do |trip|
+      if trip.cost != nil
+        total += trip.cost
+      end
+    end
+    return total
+  end
 end
