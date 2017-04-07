af088e54059e52777f6682251cb6308103c72e4f
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-04-07 15:57:07 -0700

    Cannot add new trip untill all passenger's trips rated, DONE. WE USED FLASH

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :driver, :class_name => Driver, :foreign_key => "driver_id"
   belongs_to :passenger, :class_name => Passenger, :foreign_key => "passenger_id"
+
+
+
+
 end
